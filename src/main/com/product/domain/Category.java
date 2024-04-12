cc05130ce1fe7d96b7e8f9ab14029ff17e98f702
kimdodo97 <eh4536@naver.com>
2024-04-13 00:50:33 +0900

    Feat : 카레고리 DTO 추가

--- src/main/com/product/domain/Category.java
+++ src/main/com/product/domain/Category.java
@@ -1,13 +1,17 @@
 package main.com.product.domain;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class Category {
     private Long categoryId;
     private String categoryName;
     private Long parentCategoryId;
 
+    @Builder
     public Category(Long categoryId, String categoryName, Long parentCategoryId) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
