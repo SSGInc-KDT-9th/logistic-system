c01a3bc5016a3dfd128b1bf14f11d700a9636e15
kimdodo97 <eh4536@naver.com>
2024-04-13 00:50:24 +0900

    Feat : ProductRepository 의존 주입 추가

--- src/main/com/config/AppConfig.java
+++ src/main/com/config/AppConfig.java
@@ -2,6 +2,17 @@ package main.com.config;
 
 import main.com.member.repository.MemberRepository;
 import main.com.member.repository.MySqlMemberRepository;
+import main.com.product.repository.MySqlProductRepository;
+import main.com.product.repository.ProductRepository;
+import main.com.product.service.ProductService;
+import main.com.product.service.ProductServiceImpl;
+
 public class AppConfig {
-    private MemberRepository memberRepository= new MySqlMemberRepository();
+    public ProductRepository productRepository(){
+        return new MySqlProductRepository();
+    }
+
+    public ProductService productService(){
+        return new ProductServiceImpl(productRepository());
+    }
 }
