88f3e1c9be0d72b3deed27312c6991fa1861fc80
kimdodo97 <eh4536@naver.com>
2024-04-12 16:50:41 +0900

    Test : 상품 리포지토리 테스트 구현

--- src/test/MySqlProductRepositoryTest.java
+++ src/test/MySqlProductRepositoryTest.java
@@ -1,10 +1,9 @@
 package test;
 
 import main.com.product.domain.Product;
-import main.com.product.domain.SearchDTO;
+import main.com.product.domain.ProductEdit;
 import main.com.product.repository.MySqlProductRepository;
 import main.com.product.repository.ProductRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,39 +15,35 @@ import java.util.stream.IntStream;
 class MySqlProductRepositoryTest {
     private ProductRepository productRepository = new MySqlProductRepository();
 
-    @AfterEach
+    //@AfterEach
     void clean(){
         productRepository.deleteAll();
     }
 
     @Test
-    @DisplayName("DB 삽입 조회 테스트")
+    @DisplayName("상품 ID 조회 테스트")
     void test1() throws Exception {
         //given
         Product product = Product.builder()
-                .productName("TEST")
-                .supplyPrice(100)
-                .sellingPrice(120)
-                .stock(10)
+                .productName("투게더")
+                .supplierId(1L)
+                .categoryId(2L)
                 .build();
         //when
         Long saveId = productRepository.save(product);
         Product expectProduct = productRepository.findById(saveId);
         //then
-        //Assertions.assertEquals(product.getId(),expectProduct.getId());
+        Assertions.assertEquals(product.getId(),expectProduct.getId());
         Assertions.assertEquals(product.getProductName(),expectProduct.getProductName());
-        Assertions.assertEquals(product.getSellingPrice(),expectProduct.getSellingPrice());
-        Assertions.assertEquals(product.getSupplyPrice(),expectProduct.getSupplyPrice());
     }
+
     @Test
-    @DisplayName("상품 이름으로 조회 테스트")
+    @DisplayName("상품 이름 조회 테스트")
     void test2() throws Exception {
         //given
         List<Product> products = IntStream.range(1,31)
                 .mapToObj(i-> Product.builder()
                         .productName("이름 " + i)
-                        .supplyPrice(i)
-                        .sellingPrice(i*10)
                         .build())
                 .collect(Collectors.toList());
 
@@ -59,40 +54,14 @@ class MySqlProductRepositoryTest {
         Assertions.assertEquals(expects.size(),30);
         Assertions.assertEquals(saveIds.get(0),expects.get(0).getId());
     }
-
-    @Test
-    @DisplayName("상품 공급 기업명 조회 테스트")
-    void test3() throws Exception {
-        //given
-        List<Product> products = IntStream.range(1,31)
-                .mapToObj(i-> Product.builder()
-                        .productName("이름 " + i)
-                        .supplierId(1L)
-                        .supplyPrice(i)
-                        .sellingPrice(i*10)
-                        .build())
-                .collect(Collectors.toList());
-        SearchDTO search = SearchDTO.builder()
-                .supplyName("빙그레").build();
-        //when
-        List<Long> saveIds = productRepository.save(products);
-        List<Product> expects = productRepository.findProduct(search);
-        //then
-        Assertions.assertEquals(expects.size(),30);
-        Assertions.assertEquals(expects.get(0).getProductName(), products.get(0).getProductName());
-    }
-
     @Test
     @DisplayName("상품 저장 테스트")
-    void test4() throws Exception {
+    void test3() throws Exception {
         //given
         Product product = Product.builder()
                 .productName("투게더")
                 .categoryId(2L)
                 .supplierId(1L)
-                .supplyPrice(3000)
-                .sellingPrice(5000)
-                .stock(100)
                 .build();
         Long saveId = productRepository.save(product);
         //when
@@ -102,18 +71,18 @@ class MySqlProductRepositoryTest {
         Assertions.assertEquals(expectProduct.getProductName(),product.getProductName());
         Assertions.assertEquals(expectProduct.getCategoryId(),product.getCategoryId());
         Assertions.assertEquals(expectProduct.getSupplierId(),product.getSupplierId());
+        productRepository.delete(saveId);
     }
 
     @Test
-    @DisplayName("상품 여러개 동시 저장 테스트")
-    void test5() throws Exception {
+    @DisplayName("상품 여러개 저장 테스트")
+    void test4() throws Exception {
         //given
         List<Product> products = IntStream.range(0,30)
                 .mapToObj(i-> Product.builder()
+                        .categoryId(2L)
                         .productName("이름 " + i)
                         .supplierId(1L)
-                        .supplyPrice(i)
-                        .sellingPrice(i*10)
                         .build())
                 .collect(Collectors.toList());
         List<Long> saveIds = productRepository.save(products);
@@ -127,5 +96,34 @@ class MySqlProductRepositoryTest {
             Assertions.assertEquals(expectProduct.getCategoryId(),product.getCategoryId());
             Assertions.assertEquals(expectProduct.getSupplierId(),product.getSupplierId());
         }
+        productRepository.delete(saveIds);
+    }
+
+    @Test
+    @DisplayName("상품 수정 테스트")
+    void test5() throws Exception {
+        //given
+        Product product = Product.builder()
+                .productName("투게더")
+                .categoryId(4L)
+                .supplierName("빙그레")
+                .supplierId(1L)
+                .build();
+        Long saveId = productRepository.save(product);
+        product = productRepository.findById(saveId);
+        //when
+        ProductEdit.ProductEditBuilder editBuilder = product.toEditor();
+        ProductEdit productEdit = editBuilder.productName(product.getProductName())
+                .supplierName(product.getSupplierName())
+                .supplierId(product.getSupplierId())
+                .categoryId(3L)
+                .build();
+        product.edit(productEdit);
+        Long updateId = productRepository.update(product);
+        Product expect = productRepository.findById(updateId);
+        productRepository.delete(updateId);
+        //then
+        Assertions.assertEquals(3L,expect.getCategoryId());
+        Assertions.assertNotEquals(4L,expect.getCategoryId());
     }
 }
